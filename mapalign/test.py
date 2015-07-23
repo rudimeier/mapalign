22c782e5cccbf823e5d2db2ffed44e55b2314015
Ruediger Meier <ruediger.meier@ga-group.nl>
2015-07-23 10:14:55 +0200

    test.py don't use numpy.loadtxt() to safe a lot of memory

--- mapalign/test.py
+++ mapalign/test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import embed
 import sys
+import csv
 
 
 def random_matrix(n):
@@ -10,6 +11,15 @@ def random_matrix(n):
             tmp[i,j] = tmp[j,i]
     return tmp
 
+def load_matrix(file, n):
+    b = np.zeros((n,n))
+    with open(file, 'r') as f:
+        reader = csv.reader(f,'excel-tab')
+        for i, row in enumerate(reader):
+            b[i,:] = np.array(row)
+    return b
+
+
 n = int(sys.argv[1])
 
 matrixfile = "matrix-%08d.dat" % (n)
@@ -18,7 +28,8 @@ outfile_result = "out-result-%%s-%08d.dat" % (n)
 
 #L = random_matrix(n)
 #np.savetxt(matrixfile, L, delimiter='\t', fmt='%.2f')
-L = np.loadtxt(matrixfile)
+#L = np.loadtxt(matrixfile)
+L = load_matrix(matrixfile, n)
 
 (embedding, result) = embed.compute_diffusion_map(L)
 np.savetxt(outfile_embedding, embedding, delimiter='\t', fmt='%.2f')
